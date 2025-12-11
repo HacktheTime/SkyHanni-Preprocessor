62253aa1e596d5abaa758d2f409af141a1eef33c
HacktheTime <l4bg0jb7@duck.com>
2025-12-11 19:50:52 +0100

    modified Remap dependency too use HacktheTime repo for testing purposes.

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,7 +50,7 @@ repositories {
 dependencies {
     implementation(gradleApi())
     implementation(localGroovy())
-    implementation("com.github.HacktheTime:SkyHanni-Remap:hackthetime-repo-SNAPSHOT")
+    implementation("com.github.HacktheTime:SkyHanni-Remap:1.1.1")
     implementation("net.fabricmc:mapping-io:0.8.0")
     testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
